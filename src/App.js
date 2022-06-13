e9a2dfc25db83c86091ad8f6957bacbeddf65b07
shrey619 <shreyansh19101@iiitnr.edu.in>
2022-06-14 01:25:51 +0530

    support done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import About from "./components/About";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Quiz from "./components/Quiz";
+import Service from "./components/Service";
+import Support from "./components/Support";
 
 function App() {
   return (
  <>
  <Navbar/>
+ <Hero/>
+ <Service/>
+ <About/>
+ <Support/>
+
+ {/* <Quiz/> */}
  </>
   );
 }
